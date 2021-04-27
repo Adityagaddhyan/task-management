0e3439e63cab25219d8d62d1faf835f0fe9d769d
adityagaddhyan <adityagaddhyan@gmail.com>
2021-04-27 15:29:21 +0530

    feat: create task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { Tasks } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService:TasksService){}
     @Get()
-    getAllTasks(){
+    getAllTasks():Tasks[]{
         return this.taskService.getAllTasks();
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
-
+import { Tasks, TaskStatus } from './tasks.model';
+import { vi as uuid } from 'uuid';
 @Injectable()
 export class TasksService {
-    private tasks=[];
-    getAllTasks(){
-        return this.tasks;
-    }
+  private tasks: Tasks[] = [];
+  getAllTasks() {
+    return this.tasks;
+  }
+  createTask(title: string, description: string) {
+    const task: Tasks = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
 }
